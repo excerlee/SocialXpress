cdc480c9ff94d23977d33b7a5397f437adbce3de
Joe <joe@xcomputing.net>
2011-06-19 02:27:28 -0700

    Reorganized the dir structure, and added some FB app config file.

---
+++ XStore/AppBasic/app_conf.php
@@ -0,0 +1,13 @@
+<?php
+require_once("../conf/global_conf.php");
+
+$app_canvas_url = "http://a.xcomputing.net:8081/~joe/AppBasic/";
+
+$fb_conf = array(
+  'appId' => '231766590182305',
+  'secret' => '1c53dc28d1aa0423755ee27d132ab61d',
+  'cookie' => true,
+);
+
+
+
---
+++ XStore/conf/global_conf.php
@@ -0,0 +1,2 @@
+<?php
+require_once ("./fb_config.php");
