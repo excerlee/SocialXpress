d5824f04b7f7648f27e75c97a5c63dbd5eca526c
Joe Li <joe@xcomputing.net>
2011-07-20 00:12:56 -0700

    Updated deploy script

--- XStore/tools/deploy-local-mac.sh
+++ XStore/tools/deploy-local-mac.sh
@@ -2,13 +2,17 @@
 APP=${1:?Please specify the FB app you want to deploy}
 #WEB_ROOT=$HOME/Sites
 WEB_ROOT=/Library/WebServer/Documents
-APP_DIR=$WEB_ROOT/$APP
+BAK_ROOT=$WEB_ROOT/archive
 
-BAK_DIR=${APP_DIR}-`date +%F-%H%M`
+APP_DIR=$WEB_ROOT/$APP
+BAK_DIR=$BAK_ROOT/${APP}-`date +%F-%H%M`
+
 mv $APP_DIR $BAK_DIR
-echo "Backed up old $APP to $BAK_DIR" 
+echo "Done with backing up old $APP to $BAK_DIR" 
 
 mkdir -p $APP_DIR
+mkdir -p $BAK_DIR
+
 cp -r ../$APP/* $APP_DIR/
 cp -r ../conf ../lib $WEB_ROOT
 echo "DONE with copying files to $APP_DIR" 
