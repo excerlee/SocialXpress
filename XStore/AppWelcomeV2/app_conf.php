743c1d185af7b071e64c2c751edfed77ade1ca4a
Joe Li <joe@xcomputing.net>
2011-08-09 21:43:06 -0700

    Created AppWelcomeV2, which supports single-use coupon code.

---
+++ XStore/AppWelcomeV2/app_conf.php
@@ -0,0 +1,37 @@
+<?php
+//require_once("../conf/global_conf.php");
+
+$app_canvas_url = "http://localhost/AppBasic/";
+
+$fb_config = array(
+  'appName' => 'Basic Store',
+  'appid' => '231766590182305',
+  'secret' => '1c53dc28d1aa0423755ee27d132ab61d',
+  'cookie' => true,
+  'baseurl' => 'http://ec2-50-17-121-184.compute-1.amazonaws.com/AppWelcome/index.php',
+);
+
+//The config below should be stored in a database 
+$store_config = array(
+    'storeName' => 'Brilliant Store',
+    //Welcome images
+    'welcomeImages'      => array(1=>'http://www.brilliantstore.com/promotion/facebook/banner3.jpg'),
+    //Welcome images action URLS
+    'welcomeImageTargetUrls'  => array(1=>false),
+
+    //FAN page images
+    'fanPageImages' 	 => array(1=>'http://www.brilliantstore.com/promotion/facebook/img/t27_1.jpg',
+				  2=>'http://www.brilliantstore.com/promotion/facebook/img/t27_2.jpg',
+				  3=>'http://www.brilliantstore.com/promotion/facebook/img/t27_3.jpg',
+				 ),
+    //FAN page images action URLs
+    'fanPageImageTargetUrls'=> array(1=>'http://www.brilliantstore.com/in-ear-headphones-os-ep0051.html',
+				  2=>'http://www.brilliantstore.com/other-office-equipment-os-hq0014.html',
+				  3=>'http://www.brilliantstore.com/',
+				 ),
+    'couponWSUrl'=>'http://www.brilliantstore.com/mycart/getfbcouponcode.php',
+    'couponWSToken'=>'5fb687e254e3367a96849622a53d1aab',
+
+);
+
+include("./app_conf.overrides.php");
---
+++ XStore/AppWelcomeV2/index.php
@@ -0,0 +1,116 @@
+<?php
+require_once("./common.php");
+require_once('./app_conf.php');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title><?php echo $store_config['storeName']; ?></title>
+
+
+</head>
+<body>
+<?php 
+
+$user            =   null; //facebook user uid
+
+// Create our Application instance.
+$facebook = new Facebook(array(
+  'appId'  => $fb_config['appid'],
+  'secret' => $fb_config['secret'],
+  'cookie' => true,
+));
+
+//Facebook Authentication part
+$user       = $facebook->getUser();
+
+$req = $facebook->getSignedRequest();
+
+function parsePageSignedRequest( $req ) {
+//    if (isset($_REQUEST['signed_request'])) {
+      if (isset($req)) {
+          $encoded_sig = null;
+          $payload = null;
+          list($encoded_sig, $payload) = explode('.', $_REQUEST['signed_request'], 2);
+          $sig = base64_decode(strtr($encoded_sig, '-_', '+/'));
+          $data = json_decode(base64_decode(strtr($payload, '-_', '+/'), true));
+          return $data;
+    }
+    return false;
+}
+
+function renderPage($imgList, $urlList) {
+    global $store_config;
+    foreach ( $store_config[$imgList] as $id=>$imgUrl) :
+		if ($store_config[$urlList][$id]) :?>
+			<a target='_blank' href="<?php echo $store_config[$urlList][$id] ;?>">
+				<img src="<?php echo $imgUrl ;?>" width=490>
+			</a>
+		<?php else: ?>
+			<img src="<?php echo $imgUrl ;?>" width=490>
+		<?php endif; ?>
+	<?php endforeach; 
+}
+
+
+function doCUrlCall($url) {
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+	$result = curl_exec($ch);
+    
+	return $result;
+}
+
+function getCouponCode($fbId, $fbEmail) {
+
+    global $store_config;
+    
+    $couponCode=null;
+    
+    //Example brilliant store URL:
+    // http://www.brilliantstore.com/mycart/getfbcouponcode.php?ftoken=5fb687e254e3367a96849622a53d1aab&fid={facebook id}&femail={facebook email}
+    $url = $store_config['couponWSUrl'].
+    		'?ftoken='.$store_config['couponWSToken'].
+            '&fid'=.$fbId
+            '&femail'=.$fbEmail;
+    
+    $result = json_decode(doCUrlCall($url));
+    if ( $result['s'] == 's') {
+        $couponCode = $result['c']; 
+    }else{
+        $storeName = $store_config['storeName'];
+        $couponCode = <<QQQ
+<h2>Looks like there is some problem with the system, please refresh the page and try again. 
+If still not working, please contact {$storeName}
+QQQ;
+    }
+    return $couponCode;
+    
+}
+
+
+$code = getCouponCode();
+
+if ($signed_request = parsePageSignedRequest($req)) {
+    
+    if ($signed_request->page->liked) {
+        $imgListName = 'fanPageImages';
+        $urlListName = 'fanPageImageTargetUrls';
+    } else {
+        $imgListName = 'welcomeImages';
+        $urlListName = 'welcomeImageTargetUrls';
+    }
+    
+    renderPage($imgListName, $urlListName);
+    
+}
+
+
+?>
+
+</body>
+</html>
