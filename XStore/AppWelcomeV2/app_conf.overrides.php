a2de437e34bc3f722e0161861259ed5b17c66a10
Joe Li <joe@xcomputing.net>
2011-08-11 05:55:50 +0000

    Removed the unneeded config file.

--- XStore/AppWelcomeV2/app_conf.overrides.php
+++
@@ -1,8 +0,0 @@
-<?php
-$fb_config = array(
-  'appName' => 'Welcome App',
-  'appid' => '228242077214281',
-  'secret' => '4d4f812c83b5244440c7bb47bdfc7909',
-  'cookie' => true,
-  'baseurl' => 'http://localhost/AppWelcome/index.php',
-);
